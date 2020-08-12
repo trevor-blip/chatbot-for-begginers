3a450036275d9a3824f02eb6c915ac4f30a7e020
Trevor Gandanhamo <gandanhamotrevor@gmail.com>
2020-08-12 20:05:29 +0200

    make it better

--- corona.py
+++ corona.py
@@ -24,10 +24,10 @@ def get_data(msg_text):
             total = data['response'][i]['cases']['total']
             total_deaths = data['response'][i]['deaths']['total']
             new_deaths = data['response'][i]['deaths']['new']
-            data_complete = x + "\n" + "Total Infected: " + str(total) + "\n" + "Active Cases: " + str(
+            data_complete = "Covid 19 Stats of " + x + "\n" + "Total Infected: " + str(total) + "\n" + "Active Cases: " + str(
                 active) + "\n" + "Total Recovered: " + str(recovered) + "\n" + "Critical Cases" + str(
-                critical) + "\n" + "New Cases: " + str(total) + "\n" + "Total Deaths: " + str(
-                total_deaths) + "\n" + "New Deaths: " + str(total)
+                critical) + "\n" + "New Cases: " + str(new_cases) + "\n" + "Total Deaths: " + str(
+                total_deaths) + "\n" + "New Deaths: " + str(new_deaths)
 
             return data_complete
             break
